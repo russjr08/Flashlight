912690f501646b1a387d0574163d9373aebc8dae
Russell Richardson <Russell@KronosAD.com>
2014-06-05 14:44:11 -0500

    Initial Commit!

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var progressBar : UIProgressView
+    
+    var progDown = false
+    
+    var progress: Float = 0.0
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Spawns this in a new thread.
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            while(true){
+//                println(self.progress)
+                if(self.progress >= 1.0){
+                    self.progDown = true
+                }
+                
+                if(self.progress <= 0){
+                    self.progDown = false
+                }
+                
+                if(self.progDown){
+                    self.progress -= 0.1
+                }else{
+                    self.progress += 0.1
+                }
+                // Run this code on the main/UI thread so that the progress bar actually updates.
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.progressBar.setProgress(self.progress, animated: true)
+                })
+                
+                
+                sleep(1)
+            }
+            
+        })
+//        println("We've scheduled the thread!")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func btnFlash(sender : UISwitch) {
+        println("YOU TOGGLED THE FUCKING BUTTON")
+//        var alert = new UIAlertView(title: "FLASH ALL THE LIGHTS", message: "Flash toggled", cancelButtonTitle: "Okay")
+        
+        var alert = UIAlertController(title: "The Damn Button", message: "You clicked the freaking button!", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "GET OUT OF MY FUCKING FACE!", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+        
+        
+        
+    }
 
 }
 
